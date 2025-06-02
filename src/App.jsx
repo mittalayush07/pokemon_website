23852942cc676fb13434d678db4e742a44937023
Ayush Mittal <mittalayush0703@gmail.com>
2025-06-02 17:11:16 +0530

    completed our pokemon website

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,10 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import { Pokemon } from './pokemon';
 
-function App() {
-  const [pokemon, setPokemon] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  const API = "https://pokeapi.co/api/v2/pokemon/pikachu";
-
-  const fetchPokemon = ()=>{
-    fetch(API)
-    .then((res)=> res.json())
-    .then((data)=>{
-      setPokemon(data);
-      setLoading(false);
-    })
-    .catch((error)=>{
-      setError(error);
-      setLoading(false);
-    })
-  }
-
-  useEffect(()=>{
-    fetchPokemon();
-  }, []);
-
-  if(loading){
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-
-  if(error){
-    return (
-      <div>
-        <h1>Error: {error.message}</h1>
-      </div>
-    )
-  }
-  console.log(pokemon.sprites.other.dream_world.front_default);
+export const App = () => {
   return (
     <section className='container'>
-      <header>
-        <h1>Lets Catch Pokemon</h1>
-      </header>
-      <ul className='card-demo'>
-        <li className='pokemon-card'>
-          <figure>
-          <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} className='pokemon-image' />
-          </figure>
-          <h1>{pokemon.name}</h1>
-          <div>
-            <p className="pokemon-info">
-              Height: <span> {pokemon.height} </span>
-            </p>
-            <p className="pokemon-info">
-              Weight: <span> {pokemon.weight} </span>
-            </p>
-            <p className="pokemon-info">
-              speed: <span> {pokemon.stats[5].base_stat} </span>
-            </p>
-          </div> 
-        </li>
-      </ul>
+      <Pokemon />
     </section>
   )
 }
-
-export default App
